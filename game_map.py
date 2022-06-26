c11211957bb89be1941fb463369b740a01ed9be4
DCMHopper <derekchristophermichaelhopper@gmail.com>
2022-06-26 18:15:29 -0400

    add fov and fog of war

--- game_map.py
+++ game_map.py
@@ -8,8 +8,20 @@ class GameMap:
 		self.height = height
 		self.tiles = np.full( (width, height), fill_value=tile_types.wall, order="F" )
 
+		self.visible = np.full( (width, height), fill_value=False, order="F" )
+		self.explored = np.full( (width, height), fill_value=False, order="F" )
+
 	def in_bounds(self, x: int, y: int) -> bool:
 		return 0 <= x < self.width and 0 <= y < self.height
 
+	"""
+	If a tile is in "visible", draw it with "light" styling
+	Else if a tile is in "explored", draw it with "dark" styling
+	Else draw it as "SHROUD"
+	"""
 	def render(self, console: Console) -> None:
-		console.tiles_rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+		console.tiles_rgb[0:self.width, 0:self.height] = np.select(
+			condlist = [self.visible, self.explored],
+			choicelist = [self.tiles["light"], self.tiles["dark"]],
+			default = tile_types.SHROUD
+		)
--- tile_types.py
+++ tile_types.py
@@ -12,19 +12,30 @@ tile_dt = np.dtype([
 		("walkable", np.bool),
 		("transparent", np.bool),
 		("dark", graphic_dt),
+		("light", graphic_dt),
 	])
 
 def new_tile(
 	*,
 	walkable: int,
 	transparent: int,
-	dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]]
+	dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
+	light: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]]
 ) -> np.ndarray:
-	return np.array( (walkable, transparent, dark), dtype=tile_dt )
+	return np.array( (walkable, transparent, dark, light), dtype=tile_dt )
+
+# unexplored, unseen tiles
+SHROUD = np.array( (ord(" "), (255, 255, 255), (0, 0, 0)), dtype=graphic_dt )
 
 floor = new_tile(
-	walkable=True, transparent=True, dark=( ord(" "), (255, 255, 255), (50, 50, 50) )
+	walkable=True,
+	transparent=True,
+	dark=( ord(" "), (255, 255, 255), (24, 12, 12) ),
+	light=( ord(" "), (255, 255, 255), (64, 72, 72) )
 )
 wall = new_tile(
-	walkable=False, transparent=False, dark=( ord(" "), (255, 255, 255), (200, 200, 200) )
+	walkable=False,
+	transparent=False,
+	dark=( ord(" "), (255, 255, 255), (108, 96, 96) ),
+	light=( ord(" "), (255, 255, 255), (192, 200, 208) )
 )
