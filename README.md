959dbb6b33ccd2e868cd93f6d16ab0b43e15143f
DCMHopper <derekchristophermichaelhopper@gmail.com>
2022-06-28 19:07:51 -0400

    refactor

---
+++ README.md
@@ -0,0 +1,7 @@
+# Strange-RL
+
+This is an ASCII roguelike based off `python-tcod` and the associated [Roguelike Tutorial](https://rogueliketutorials.com/)
+
+Conceptually this game is about walking through asteroids
+
+I'm not keeping a devlog or anything right now, because other than some flavor elements I'm following the tutorial code almost exactly. If I end up extending this game beyond that tutorial, I'll add a changelog and possibly more documentation around development
--- actions.py
+++ actions.py
@@ -1,62 +1,86 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from typing import Optional, Tuple, TYPE_CHECKING
 
 if TYPE_CHECKING:
 	from engine import Engine
 	from entity import Entity
 
+# Generic action class which all other inherit
 class Action:
-	def perform(self, engine: Engine, entity: Entity) -> None:
+	def __init__(self, entity: Entity) -> None:
+		super().__init__()
+		self.entity = entity
+
+	@property
+	def engine(self) -> Engine:
+		return self.entity.game_map.engine
+
+	def perform(self) -> None:
 		# Override this method in subclasses
 		raise NotImplementedError()
 
+
+# Quit the game
 class EscapeAction(Action):
-	def perform(self, engine: Engine, entity: Entity) -> None:
+	def perform(self) -> None:
 		raise SystemExit()
 
+
+# Generic directional action
 class ActionWithDirection(Action):
-	def __init__(self, dx: int, dy:int):
-		super().__init__()
+	def __init__(self, entity: Entity, dx: int, dy:int):
+		super().__init__(entity)
 
 		self.dx = dx
 		self.dy = dy
 
-	def perform(self, engine: Engine, entity: Entity) -> None:
+	@property
+	def dest_xy(self) -> Tuple[int, int]:
+		return self.entity.x + self.dx, self.entity.y + self.dy
+	
+
+	@property
+	def blocking_entity(self) -> Optional[Entity]:
+		return self.engine.game_map.get_blocking_entity_at_location(*self.dest_xy)
+	
+
+	def perform(self) -> None:
 		raise NotImplementedError()
 
+
+# Directional melee attack
 class MeleeAction(ActionWithDirection):
 
-	def perform(self, engine: Engine, entity: Entity) -> None:
-		dest_x = entity.x + self.dx
-		dest_y = entity.y + self.dy
-		target = engine.game_map.get_blocking_entity_at_location(dest_x, dest_y)
+	def perform(self) -> None:
+		target = self.blocking_entity
+
 		if not target:
 			return
 
 		print(f"You swing at the {target.name}!")
 
+
+# Directional movement action
 class MovementAction(ActionWithDirection):
 
-	def perform(self, engine: Engine, entity: Entity) -> None:
-		dest_x = entity.x + self.dx
-		dest_y = entity.y + self.dy
+	def perform(self) -> None:
+		dest_x, dest_y = self.dest_xy
 
-		if not engine.game_map.in_bounds(dest_x, dest_y):
+		if not self.engine.game_map.in_bounds(dest_x, dest_y):
 			return
-		if not engine.game_map.tiles["walkable"][dest_x,dest_y]:
+		if not self.engine.game_map.tiles["walkable"][dest_x,dest_y]:
 			return
-		if engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
+		if self.engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
 			return
 
-		entity.move(self.dx, self.dy)
+		self.entity.move(self.dx, self.dy)
 
+
+# Selects melee or movement based on blocking entity in target cell
 class BumpAction(ActionWithDirection):
 
-	def perform(self, engine: Engine, entity: Entity) -> None:
-		dest_x = entity.x + self.dx
-		dest_y = entity.y + self.dy
-
-		if engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
-			return MeleeAction(self.dx, self.dy).perform(engine, entity)
+	def perform(self) -> None:
+		if self.blocking_entity:
+			return MeleeAction(self.entity, self.dx, self.dy).perform()
 		else:
-			return MovementAction(self.dx, self.dy).perform(engine, entity)
+			return MovementAction(self.entity, self.dx, self.dy).perform()
